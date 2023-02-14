54eba58948732a64191f67851d9b8df34d8f67fb
Navneet Singh <navneets@beingzero.in>
2023-02-14 19:06:53 +0530

    updated the server

---
+++ public/js/main.js
@@ -0,0 +1,4 @@
+document.addEventListener("DOMContentLoaded", () => {
+    if (document.getElementById("hello"))
+        alert("html fetched")
+})
--- server.js
+++ server.js
@@ -1,7 +1,33 @@
-const app = require('./app')
+const express = require("express");
+const path = require("path");
+const app = express();
 
-const port = process.env.PORT || 3000
+const port = 3000
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+const options = {
+    extensions: ['htm', 'html','css','js','ico','jpg','jpeg','png','svg'],
+    index: ['index.html'],  
+}
+
+app.use(function (req, res, next) {
+    res.set('x-timestamp', Date.now())
+    res.set('x-powered-by', 'cyclic.sh')
+    console.log(`[${new Date().toISOString()}] ${req.ip} ${req.method} ${req.path}`);
+    next();
+});
+
+app.use(express.static("public", options));
+
+app.get("/", (req, res) => {
+    res.sendFile("index.html");
+})
+
+app.get("/about", (req, res) => {
+    res.sendFile("about.html");
+})
+
+
+
+app.listen(port, (req, res) => {
+    console.log(`Server started at http://localhost:${port}`)
 })
