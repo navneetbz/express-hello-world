2d18ec5082cdb63223ba3899da510190432eacec
Navneet Singh <navneets@beingzero.in>
2023-02-22 13:42:32 +0530

    added resume back

--- server.js
+++ server.js
@@ -22,6 +22,10 @@ app.get("/", (req, res) => {
     res.sendFile(__dirname + "/index.html");
 })
 
+app.get("/resume", (req, res) => {
+    res.sendFile(__dirname + "/public/resume.html")
+})
+
 app.get("/about", (req, res) => {
     res.sendFile(__dirname + "/public/about.html");
 })
